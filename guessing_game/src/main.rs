dfa595c6a0fb2b16130a39939e6623f9dcfd5de1
박시율 <kdpark0723@gmail.com>
2019-12-07 17:26:56 +0900

    리팩토링

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -14,8 +14,7 @@ fn main() {
 
         let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
+        io::stdin().read_line(&mut guess)
             .ok()
             .expect("Failed to read line");
 
